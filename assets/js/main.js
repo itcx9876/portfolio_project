d2f828f90688f187d97aca0836569cc95787a5d2
Hadi <abdulhadi.slow@gmail.com>
2023-01-27 08:45:13 +0500

    added js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+AOS.init({
+    duration:1000,
+    easing:'ease-in-out',
+    once:true,
+    mirror:false
+});
+// Added Slider for testimonials
+const config={
+    speed:2000,
+    loop:true,
+    autoplay:{
+        delay:500,
+        disabledOnIteraction:false
+    },
+    pagination:{
+        el: ".swiper-pagination",
+    }
+}
+new Swiper('.testimonials-slider',config)
+
 new Typed('.typed',{
     strings: ['adcsacas,acac','asdsad','zzz'],
     loop:true,
@@ -7,3 +27,23 @@ new Typed('.typed',{
 })
 
 new PureCounter();
+function add_progress_width(el){
+
+}
+function handle_waypoint(){
+    const progress_bars=document.getElementsByClassName('progress-bar')
+    // console.log(progress_bars[3].getAttribute("aria-valuenow"))
+    for (var i=0;i<progress_bars.length;i++){
+        const value=progress_bars[i].getAttribute("aria-valuenow")
+        console.log(value,"this is value")
+        progress_bars[i].style.width=value+"%"
+    }
+   
+}
+const skills_element=document.getElementsByClassName("skills-content")[0]
+if(skills_element){
+    new Waypoint({
+        element:skills_element,
+        handler:handle_waypoint
+    })
+}
